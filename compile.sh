4e5cbd8f50098760f008be73f2feff6696e55196
Forest Kobayashi <fkobayashi@hmc.edu>
2019-12-24 18:57:20 -0800

    add bmb compile script

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd hw1 && mv comments.tex m147-guidelines.tex && bmbcompile m147-guidelines.tex && cd ..
+cd hw4 && mv hw4-solutions.tex m147-hw4-sol.tex && bmbcompile m147-hw4-sol.tex && cd ..
+cd hw5 && mv hw5-solutions.tex m147-hw5-sol.tex && bmbcompile m147-hw5-sol.tex && cd ..
